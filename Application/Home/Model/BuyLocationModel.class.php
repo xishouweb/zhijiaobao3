4c54bab47ca38e691750a9dd25ce4e96dcf706b0
aqingyan <18710892127@163.com>
2017-07-29 13:13:12 +0800

    新增model

--- Application/Home/Model/BuyLocationModel.class.php
+++ Application/Home/Model/BuyLocationModel.class.php
@@ -3,6 +3,7 @@ namespace Home\Model;
 use Think\Model\RelationModel;
 class BuyLocationModel extends RelationModel{
     //地点购买表model
+    
 
 }
 ?>
---
+++ Application/Home/Model/QuestionModel.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Model;
+use Think\Model\RelationModel;
+class QuestionModel extends RelationModel{
+    //问题表model
+
+}
+?>
