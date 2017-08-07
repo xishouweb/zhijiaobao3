28182519ad758d44b68e216c759bde907e705188
aqingyan <18710892127@163.com>
2017-08-08 00:12:30 +0800

    新增地点方法，修改model

--- Application/Home/Controller/LocationController.class.php
+++ Application/Home/Controller/LocationController.class.php
@@ -3,7 +3,42 @@ namespace Home\Controller;
 use Think\Controller;
 class LocationController extends Controller {
     //地点控制器类
-    public function Location(){
+    public $location;
+
+    public function _initialize() {
+        $this->location = D('Location');
+    }
+
+    public function index(){
         $this->display();
     }
+
+    // 搜索功能
+    public function searchLocation() {
+
+        //获取变量
+        $keyword = I('key_word');  // 关键字
+        $keyword = tranSql($keyword);
+
+        $list = $this->location->getList(0,$keyword);
+        // 返回数据
+        $this->success($list);
+    }
+
+    // 地点列表
+    public function locationList() {
+        $list = $this->location->getList();
+
+        $this->success($list);
+    }
+
+    // 推荐地点列表
+    public function recommendLocal() {
+        $search = 'visit desc limit 6';
+        $list = $this->location->getList(0,'',$search);
+
+        $this->success($list);
+    }
+
+    // 地点详情
 }
--- Application/Home/Model/LocationModel.class.php
+++ Application/Home/Model/LocationModel.class.php
@@ -3,15 +3,21 @@ namespace Home\Model;
 use Think\Model\RelationModel;
 class LocationModel extends RelationModel{
     //地点表model
-
+    protected $_link = array (
+        'Community' => array(
+            'mapping_type' => self::BELONGS_TO,
+            'class_name'  => 'Community',
+            'mapping_name'=> 'community',
+            'foreign_key' => 'community_id',
+            'as_fields'   => 'community_name:community',
+        )
+    );
     //获取地点列表
-    public function getList($userId=NULL,$key=NULL) {
-        $map['1'] = 1;
+    public function getList($userId=NULL,$key=NULL,$order='create_time desc') {
         if (!empty($userId))
             $map['community_id'] = $userId;
-        if (!empty($key))
-            $map['school|city|province|address|bus_station|train_station'] = array('like','%'.$key.'%');
-        $list = $this->where($map)->select();
+        $map['school|city|province|address|bus_station|train_station'] = array('like','%'.$key.'%');
+        $list = $this->relation(true)->where($map)->order($order)->field('location_id,school,address,date_format(create_time,"%Y-%m-%d") as time,community_id,is_free,gold')->select();
         return $list;
     }
 
