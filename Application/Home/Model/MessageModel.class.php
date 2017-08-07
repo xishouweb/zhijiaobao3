6951738ee7075a22f527f25c6c2b84dd73724f6a
aqingyan <18710892127@163.com>
2017-08-07 13:07:19 +0800

    ss

--- Application/Home/Model/MessageModel.class.php
+++ Application/Home/Model/MessageModel.class.php
@@ -4,5 +4,26 @@ use Think\Model\RelationModel;
 class MessageModel extends RelationModel{
     //消息表model
 
+    //获取已发送消息
+    public function selectSendMessage($where,$order='time') {
+        return $this->where($where)->order($order)->select();
+    }
+
+    //添加消息记录
+    //插入记录后，在MessageRecord表中同时写入记录??
+    public function insertMessage($data) {
+        return $this->data($data)->add();
+    }
+
+    //更新消息记录
+    public function updateMessage($messageId,$data) {
+        $map['message_id'] = $messageId;
+        return $this->where($map)->data($data)->save();
+    }
+
+    //删除消息记录
+    public function deleteMessage($where) {
+        return $this->where($where)->delete();
+    }
 }
 ?>
